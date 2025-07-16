43a8f6185400ba1ffde8149b2f62ae9833127bb4
dinhtruong <tqdinhhcmus@gmail.com>
2025-07-16 23:17:46 +0700

    jul16

---
+++ Jul16/414. Third Maximum Number.py
@@ -0,0 +1,15 @@
+import heapq
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        seen =set()
+        min_heap=[]
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+
+                heapq.heappush(min_heap,num)
+                if len(min_heap)>3:
+                    heapq.heappop(min_heap)
+        if len(min_heap) <3:
+            return max(min_heap)
+        return min_heap[0]
